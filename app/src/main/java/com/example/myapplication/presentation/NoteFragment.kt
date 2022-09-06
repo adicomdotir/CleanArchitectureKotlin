c7bf2b201f3d2725facd900b9a76c322c9a3a9b6
adicomdotir <adicom.ir@gmail.com>
2022-09-06 18:51:40 +0430

    Add viewModel and complete NoteFragment

--- app/src/main/java/com/example/myapplication/presentation/NoteFragment.kt
+++ app/src/main/java/com/example/myapplication/presentation/NoteFragment.kt
@@ -5,13 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
+import com.example.core.data.Note
 import com.example.myapplication.R
 import com.example.myapplication.databinding.FragmentNoteBinding
+import com.example.myapplication.framework.NoteViewModel
 
 class NoteFragment : Fragment() {
     private lateinit var binding: FragmentNoteBinding
+    private lateinit var viewModel: NoteViewModel
+    private val currentNote = Note("", "", 0L, 0L)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,9 +31,34 @@ class NoteFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProvider(this).get(NoteViewModel::class.java)
+
         binding.checkButton.setOnClickListener {
-            Navigation.findNavController(it).popBackStack()
+            if (binding.titleView.text.isNotEmpty() || binding.contentView.text.isNotEmpty()) {
+                val time = System.currentTimeMillis()
+                currentNote.title = binding.titleView.text.toString()
+                currentNote.content = binding.contentView.text.toString()
+                currentNote.updateTime = time
+                if (currentNote.id == 0L) {
+                    currentNote.creationTime = time
+                }
+                viewModel.savedNote(currentNote)
+            } else {
+                Navigation.findNavController(it).popBackStack()
+            }
         }
 
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.saved.observe(viewLifecycleOwner, Observer {
+            if (it) {
+                Toast.makeText(context, "Done!", Toast.LENGTH_SHORT).show()
+                Navigation.findNavController(binding.titleView).popBackStack()
+            } else {
+                Toast.makeText(context, "Something went wrong, please try again", Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
